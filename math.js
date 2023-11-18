754e1a2eea69acd49cef20a30e4a9278739aded7
MahfuzurRahman99 <mahfuzz999@gmail.com>
2023-11-18 15:26:19 +0600

    2nd commit

---
+++ math.js
@@ -0,0 +1,14 @@
+var eggPrice = 14;
+var numberOfEggs = 5;
+console.log(eggPrice * numberOfEggs);
+
+
+var personOne = "Jamal";
+var personTwo = "Kamal";
+var areTheyBrothers = false;
+
+if (personOne == "Jamal" , personTwo == "Kamal" , areTheyBrothers){
+    console.log("They are brothers");
+}else {
+    console.log("They are not brothers");
+};
